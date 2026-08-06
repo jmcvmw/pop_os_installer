21ddd36c1c5ebdfed071d378cec7fbeeec6187f8
jason meers <register@jasonmeers.com>
2026-08-06 18:18:20 +0100

    updated

--- 60-nvidia-rtx-1060.sh
+++ 60-nvidia-rtx-1060.sh
@@ -1,14 +1,14 @@
 echo -e "\e[36m====== NVIDIA-RTX-1060 BEGIN =====\e[0m"
 
 
-echo -e "\e[33m====== START nvidia-driver-470 =====\e[0m"
-sudo apt install nvidia-driver-470 -y
-echo -e "\e[32m====== END nvidia-driver-470 =====\e[0m"
+echo -e "\e[33m====== START nvidia-driver-580 =====\e[0m"
+sudo apt install nvidia-driver-580 -y
+echo -e "\e[32m====== END nvidia-driver-580 =====\e[0m"
 
 
-echo -e "\e[33m====== START nvidia-utils-470 =====\e[0m"
-sudo apt install nvidia-utils-470 -y
-echo -e "\e[32m====== END nvidia-utils-470 =====\e[0m"
+echo -e "\e[33m====== START nvidia-utils-580 =====\e[0m"
+sudo apt install nvidia-utils-580 -y
+echo -e "\e[32m====== END nvidia-utils-580 =====\e[0m"
 
 
 echo -e "\e[33m====== START nvtop =====\e[0m"
