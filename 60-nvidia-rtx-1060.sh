c5d6709a028d9db292bac4217c0117a88612a8b4
jason meers <register@jasonmeers.com>
2026-08-06 18:09:32 +0100

    formatting

--- 60-nvidia-rtx-1060.sh
+++ 60-nvidia-rtx-1060.sh
@@ -1,13 +1,29 @@
-echo -e "\e[33m====== Installing NVIDIA driver 470 =====\e[0m"
+echo -e "\e[36m====== NVIDIA-RTX-1060 BEGIN =====\e[0m"
+
+
+echo -e "\e[33m====== START nvidia-driver-470 =====\e[0m"
 sudo apt install nvidia-driver-470 -y
+echo -e "\e[32m====== END nvidia-driver-470 =====\e[0m"
 
 
-echo -e "\e[33m====== Installing NVIDIA driver 470 =====\e[0m"
+echo -e "\e[33m====== START nvidia-utils-470 =====\e[0m"
 sudo apt install nvidia-utils-470 -y
+echo -e "\e[32m====== END nvidia-utils-470 =====\e[0m"
 
-echo -e "\e[33m====== Installing nvtop =====\e[0m"
+
+echo -e "\e[33m====== START nvtop =====\e[0m"
 sudo apt install nvtop -y
+echo -e "\e[32m====== END nvtop =====\e[0m"
 
-echo -e "\e[33m====== Installing NVIDIA CUDA toolkit =====\e[0m"
+
+echo -e "\e[33m====== START NVIDIA CUDA toolkit =====\e[0m"
 sudo apt install nvidia-cuda-toolkit -y
-echo -e "\e[31mRED====== Installing NVIDIA CUDA toolkit =====\e[0m" 
+echo -e "\e[32m====== END NVIDIA CUDA toolkit =====\e[0m" 
+
+
+echo -e "\e[36m====== NVIDIA-RTX-1060 END =====\e[0m"
+
+
+#echo -e "\e[33m====== TEXT COLOURS - 33 Yellow =====\e[0m"
+#echo -e "\e[32m====== TEXT COLOURS - 32 Green =====\e[0m"
+#echo -e "\e[36m====== TEXT COLOURS - 36 Cyan =====\e[0m"
