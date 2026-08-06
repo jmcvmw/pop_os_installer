e375d430981664d801c040fc54b51d6233c12084
jason meers <register@jasonmeers.com>
2026-08-06 18:03:02 +0100

    updates text colours

---
+++ 100-text-colours.sh
@@ -0,0 +1,41 @@
+echo -e "\e[30m====== TEXT COLOURS - 30 Dark Grey =====\e[0m"
+
+
+echo -e "\e[31m====== TEXT COLOURS - 31 Red =====\e[0m"
+
+
+echo -e "\e[32m====== TEXT COLOURS - 32 Green =====\e[0m"
+
+
+echo -e "\e[33m====== TEXT COLOURS - 33 Yellow =====\e[0m"
+
+
+echo -e "\e[34m====== TEXT COLOURS - 34 Blue =====\e[0m"
+
+
+echo -e "\e[35m====== TEXT COLOURS - 35 Magenta =====\e[0m"
+
+
+echo -e "\e[36m====== TEXT COLOURS - 36 Cyan =====\e[0m"
+
+
+echo -e "\e[37m====== TEXT COLOURS - 37 White =====\e[0m"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 60-nvidia-rtx-1060.sh
+++ 60-nvidia-rtx-1060.sh
@@ -1,5 +1,13 @@
+echo -e "\e[33m====== Installing NVIDIA driver 470 =====\e[0m"
 sudo apt install nvidia-driver-470 -y
+
+
+echo -e "\e[33m====== Installing NVIDIA driver 470 =====\e[0m"
 sudo apt install nvidia-utils-470 -y
+
+echo -e "\e[33m====== Installing nvtop =====\e[0m"
 sudo apt install nvtop -y
+
+echo -e "\e[33m====== Installing NVIDIA CUDA toolkit =====\e[0m"
 sudo apt install nvidia-cuda-toolkit -y
- 
+echo -e "\e[31mRED====== Installing NVIDIA CUDA toolkit =====\e[0m" 
