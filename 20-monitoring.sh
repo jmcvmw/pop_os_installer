4cab2e87ab60428414d31c4398c7d8b94114e6e8
jason meers <register@jasonmeers.com>
2026-08-06 14:57:12 +0100

    adding monitoring tools

---
+++ 20-monitoring.sh
@@ -0,0 +1,19 @@
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install lm-sensors -y
+sudo apt install htop -y
+sudo apt install iftop -y
+sudo apt install iotop -y
+sudo apt install net-tools -y
+sudo apt install smartmontools -y
+#sudo apt install inxi -y
+#sudo apt install gnome-system-monitor -y    
+#sudo apt install glances -y
+#sudo apt install sysstat -y
+#sudo apt install bmon -y
+#sudo apt install nload -y
+#sudo apt install vnstat -y
+#sudo apt install speedtest-cli -y
+
+flatpak install app/io.missioncenter.MissionCenter
