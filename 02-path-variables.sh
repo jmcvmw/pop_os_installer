85aa84b62ea43c2cbeea483c601b4bc47f1ca711
jason meers <register@jasonmeers.com>
2026-08-08 00:31:27 +0100

    added path commands

--- 02-path-variables.sh
+++ 02-path-variables.sh
@@ -1,2 +1,13 @@
 PATH="${PATH:+${PATH}:}~/opt/rocm-7.2.4/bin"
 
+
+
+
+
+
+# .bash.rc
+
+# Added by LM Studio CLI (lms)
+#export PATH="$PATH:/home/jason/.lmstudio/bin"
+# End of LM Studio CLI section
+#PATH="${PATH:+${PATH}:}~/opt/rocm-7.2.4/bin"
